5dcfa0c1e83c15d2941359940043fac8e6cf4b2e
chitano kumar <chitanokumar@gmail.com>
2018-04-26 11:04:02 +0600

    line template message

--- src/LineMessage.php
+++ src/LineMessage.php
@@ -6,26 +6,48 @@ use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+// For Template message
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+
+
 
 /**
  * summary
  */
 class LineMessage
 {
-	private $bot;
-	private $client;
+    private $bot;
+    private $client;
     private $messageBuilder;
+    private $actionBuilders;
+    private $templateBuilder;
+    private $template;
     /**
      * summary
      */
     public function __construct()
     {
-    	$this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
-    	$this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
+        $this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
+        $this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
+        $this->actionBuilders = [];
+
     }
 
     public function replyMessage($replayToken, $replyText){
-    	$this->bot->replyText($replayToken, $replyText);
+        $this->bot->replyText($replayToken, $replyText);
     }
 
     public function pushMessage($to, $text){
@@ -33,6 +55,85 @@ class LineMessage
         $response = $this->bot->pushMessage($to, $textMessageBuilder);
         return $response;
     }
+
+    public function GroupMessage($title, $text, $imageUrl, $url){
+        $to = "C7da644e0923d47d5bfc0ef53bf98cce7";
+        $buttonTemplateBuilder = new ButtonTemplateBuilder(
+                    "$title",
+                    "$text",
+                    "$imageUrl",
+                    [
+                        new UriTemplateActionBuilder("Show Details", "$url"),
+                    ]
+                );
+        $buttonMessageTemp = new TemplateMessageBuilder("Property Notification", $buttonTemplateBuilder);
+        $response = $this->bot->pushMessage($to, $buttonMessageTemp);
+        return $response;
+    }
+
+    public function GroupMessageCarousel($title = array(), $text = array(), $imageUrl = array(), $url){
+        $to = "C7da644e0923d47d5bfc0ef53bf98cce7";
+            $carouselTemplateBuilder = new CarouselTemplateBuilder([
+                    new CarouselColumnTemplateBuilder("$title[0]", "$text[0]", $imageUrl[0], [
+                        new UriTemplateActionBuilder("View Details", "$url"),
+                    ]),
+                    new CarouselColumnTemplateBuilder("$title[1]", "$text[1]", $imageUrl[1], [
+                        new PostbackTemplateActionBuilder("New Action", "action=add&itemid=123"),
+                    ]),
+                    new CarouselColumnTemplateBuilder("$title[2]", "$text[2]", $imageUrl[2], [
+                        new PostbackTemplateActionBuilder("Add to cart", "action=add&itemid=123"),
+                    ]),
+                ]);
+                $templateMessage = new TemplateMessageBuilder('Alt Text', $carouselTemplateBuilder);
+                $this->bot->pushMessage($to, $templateMessage);
+    }
+
+    public function GroupCarouselMessage($title = array(), $text = array(), $imageUrl = array(), $url = array(), $to = ""){
+            // $to = "C7da644e0923d47d5bfc0ef53bf98cce7";
+                $carouselTemplateBuilder = "";
+                if(count($imageUrl) < 2){
+                $carouselTemplateBuilder = new CarouselTemplateBuilder([
+                    new CarouselColumnTemplateBuilder("$title[0]", "$text[0]", $imageUrl[0], [
+                        new UriTemplateActionBuilder("View Details", "$url[0]"),
+                    ]),
+                    new CarouselColumnTemplateBuilder("$title[1]", "$text[1]", $imageUrl[0], [
+                        new UriTemplateActionBuilder("LINE ME", "$url[1]"),
+                    ]),
+                    new CarouselColumnTemplateBuilder("$title[2]", "$text[2]", $imageUrl[0], [
+                        new UriTemplateActionBuilder("LINE JERBIT", "$url[2]"),
+                    ]),
+                ]);
+            }elseif(count($imageUrl) < 3 ){
+                $carouselTemplateBuilder = new CarouselTemplateBuilder([
+                    new CarouselColumnTemplateBuilder("$title[0]", "$text[0]", $imageUrl[0], [
+                        new UriTemplateActionBuilder("View Details", "$url[0]"),
+                    ]),
+                    new CarouselColumnTemplateBuilder("$title[1]", "$text[1]", $imageUrl[1], [
+                        new UriTemplateActionBuilder("LINE ME", "$url[1]"),
+                    ]),
+                    new CarouselColumnTemplateBuilder("$title[2]", "$text[2]", $imageUrl[0], [
+                        new UriTemplateActionBuilder("LINE JERBIT", "$url[2]"),
+                    ]),
+                ]);
+            }else{
+                $carouselTemplateBuilder = new CarouselTemplateBuilder([
+                    new CarouselColumnTemplateBuilder("$title[0]", "$text[0]", $imageUrl[0], [
+                        new UriTemplateActionBuilder("View Details", "$url[0]"),
+                    ]),
+                    new CarouselColumnTemplateBuilder("$title[1]", "$text[1]", $imageUrl[1], [
+                        new UriTemplateActionBuilder("LINE ME", "$url[1]"),
+                    ]),
+                    new CarouselColumnTemplateBuilder("$title[2]", "$text[2]", $imageUrl[2], [
+                        new UriTemplateActionBuilder("LINE JERBIT", "$url[2]"),
+                    ]),
+                ]);
+            }
+
+            $templateMessage = new TemplateMessageBuilder('Alt Text', $carouselTemplateBuilder);
+            $this->bot->pushMessage($to, $templateMessage);
+    }
+
+
 
     public function getProfile($id){
         $response = $this->bot->getProfile($id);
@@ -86,4 +187,5 @@ class LineMessage
           ], 200);
 
     }
+
 }
